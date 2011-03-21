756c62ed098bda5d9e607e4d2a0c6ca9ea77bf71
Stephen Becker IV <sbeckeriv+git@gmail.com>
2011-03-20 17:29:23 -0700

    configurable outputs and create the folders

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,5 +1,7 @@
 pref("extensions.textport.oneMesageFile", false);
 pref("extensions.textport.template", "");
+pref("extensions.textport.contacts_path", "");
+pref("extensions.textport.messages_path", "");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.becker@deathbyescalator.com.description", "chrome://textport/locale/overlay.properties");
